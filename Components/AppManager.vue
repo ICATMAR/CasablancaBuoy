e1c542ea458be653d587ab5f654a1a0ca3a91656
Gerard Llorach <gerard.llorach@gmail.com>
2023-09-05 12:03:54 +0200

    added info icon, now working on language selector

--- Components/AppManager.vue
+++ Components/AppManager.vue
@@ -21,19 +21,28 @@
 
 
 
-    <!-- ICONS -->
+    <!-- LEFT ICONS -->
     <a href="https://icatmar.cat/">
       <img class="logo clickable icatmar-logo" src="Assets/Logos/icatmar-mini-logo.svg">
     </a>
     <a href="https://icatmar.cat/visors/xarxa-observacional/">
       <img class="logo clickable obs-logo" src="Assets/Logos/ocea-mini-1.svg">
     </a>
-    <!-- Repository -->
-    <a href="https://github.com/ICATMAR/CasablancaBuoy" target="_blank">
-      <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="logo clickable github-logo">
-          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-      </svg>
-    </a>
+
+    <!-- RIGHT ICONS -->
+    <div class="top-right-icons-container">
+      <a href="https://github.com/ICATMAR/CasablancaBuoy" target="_blank">
+        <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="clickable github-logo">
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+        </svg>
+      </a>
+
+      <div class="clickable icon-str" @click="infoIconClicked">i</div>
+
+      <language-selector></language-selector>
+
+    </div>
+    
   </div>
 </template>
 
@@ -55,6 +64,7 @@ import InstrumentsMenu from "./Instruments/InstrumentsMenu.vue"
 import TopRightNav from "./TopRight/TopRightNav.vue"
 import BottomSection from "./Bottom/BottomSection.vue"
 import CentralPanel from "./CentralPanel.vue"
+import LanguageSelector from "/CasablancaBuoy/Components/TopRight/LanguageSelector.vue"
 
 export default {
   name: "AppManager",
@@ -76,6 +86,9 @@ export default {
   },
   methods: {
     //onclick: function(e){},
+    infoIconClicked: function(){
+      window.eventBus.emit('OpenCentralPanel', 'infoPanel');
+    }
   },
   components: {
     "canvas3D": Canvas3D,
@@ -83,6 +96,7 @@ export default {
     "top-right-nav": TopRightNav,
     "bottom-section": BottomSection,
     "central-panel": CentralPanel,
+    "language-selector": LanguageSelector,
   }
 }
 </script>
@@ -117,6 +131,26 @@ export default {
   left: clamp(110px, 9vw, 140px);
 }
 
+.top-right-icons-container{
+  position: absolute;
+  top: 6px;
+  right: 10px;
+  background: red;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+
+.top-right-icons-container > * {
+  width: 28px;
+  height: 28px;
+
+  padding: 0px;
+  margin: 5px;
+  z-index: 10;
+}
+
 .github-logo {
   background: white;
   border-radius: 50%;
@@ -125,8 +159,6 @@ export default {
   border: double;
   width: 28px;
   height: 28px;
-
-  right: 35px;
-  top: 6px;
 }
+
 </style>
