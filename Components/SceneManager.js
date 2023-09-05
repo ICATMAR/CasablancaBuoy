fbf3f1044533a50daea87e290b83aca0dadf7c90
Gerard Llorach <gerard.llorach@gmail.com>
2023-09-05 16:59:13 +0200

    hidden obsea objects. now problems with flag scaling

--- Components/SceneManager.js
+++ Components/SceneManager.js
@@ -118,21 +118,22 @@ class SceneManager{
     // OBSEA Buoy
     this.casablancaBuoy = new CasablancaBuoyEntity(scene);
     // OBSEA Base
-    this.obseaBase = new OBSEAStationEntity(scene);
+    //this.obseaBase = new OBSEAStationEntity(scene);
     // OBSEA Biotop
-    this.obseaBiotop = new OBSEABiotopEntity(scene);
+    //this.obseaBiotop = new OBSEABiotopEntity(scene);
     // OBSEA Crawler
-    this.obseaCrawler = new OBSEACrawlerEntity(scene);
+    //this.obseaCrawler = new OBSEACrawlerEntity(scene);
 
     // Flag
     this.flag = new FlagEntity(scene, () => {
-      this.flag.root.position.y = 1.3;
+      this.flag.root.position.y = 2;
+      this.flag.root.scale.set(1,1,1);
     });
     
 
     // Rosa dels vents
     this.rosaVents = new RosaVentsEntity(scene);
-    this.rosaVents.root.position.y = 7;
+    this.rosaVents.root.position.y = 3;
     
     // Sea velocity, currents
     this.currents = new CurrentEntity(scene);
@@ -143,29 +144,20 @@ class SceneManager{
     // SURFACE
     // Wind text mesh
     this.windText = new TextMeshEntity(scene, "", 0.25, 0x000000, () => {
-      this.windText.textObj.position.y = 9;
+      this.windText.textObj.position.y = 3;
     });
     // Orientation text meshes
-    this.Ntext = new TextMeshEntity(scene, "N", 0.8, 0xff0000, () => {
+    this.Ntext = new TextMeshEntity(scene, "N", 0.5, 0xff0000, () => {
       this.Ntext.textObj.rotation.x = -Math.PI / 2;
       this.Ntext.textObj.position.y = 1;
-      this.Ntext.textObj.position.z = -8;
+      this.Ntext.textObj.position.z = -4;
     });
-    this.Stext = new TextMeshEntity(scene, "S", 0.8, 0xffffff, () => {
+    this.Stext = new TextMeshEntity(scene, "S", 0.5, 0xffffff, () => {
       this.Stext.textObj.rotation.x = -Math.PI / 2;
       this.Stext.textObj.position.y = 1;
-      this.Stext.textObj.position.z = 8;
+      this.Stext.textObj.position.z = 4;
     });
-    // BOTTOM
-    // Temperature text
-    this.tempText = new TextMeshEntity(scene, "16º C", 0.25, 0x000000, () => {
-      this.tempText.textObj.rotation.x = -Math.PI / 2;
-      this.tempText.textObj.position.set(3, -19.35, 0);
-    });
-    this.salText = new TextMeshEntity(scene, "37.8 psu", 0.25, 0x000000, () => {
-      this.salText.textObj.rotation.x = -Math.PI / 2;
-      this.salText.textObj.position.set(3, -19.35, 0.5);
-    });
+
     // Potential salinity
 
     // OBSEA Data
